ca27425e56a99a358d16c01855fb18186b8d684f
Tan1 <tanyimailcn@gmail.com>
2022-09-18 23:07:18 +0800

    ver 0.1.0

--- Account_mac.py
+++ Account_mac.py
@@ -2,7 +2,6 @@ import pandas as pd
 import openpyxl
 from datetime import datetime
 
-
 def strip_in_data(data):  # 把列名中和数据中首尾的空格都去掉。
     data = data.rename(columns={column_name: column_name.strip() for column_name in data.columns})
     data = data.applymap(lambda x: x.strip().strip('¥') if isinstance(x, str) else x)
@@ -13,8 +12,8 @@ def read_data_ccb1(path):  # 获取建设银行储蓄卡数据
     ccb1 = ccb1.iloc[:, [1, 2, 3, 4, 7, 9, 10]]  # 按顺序提取所需列
     ccb1 = strip_in_data(ccb1)  # 去除列名与数值中的空格。
     ccb1.iloc[:, 0] = ccb1.iloc[:, 0].astype('str')  # 数据类型更改
-    for i in ccb1.index:
-        ccb1['交易日期'][i] = datetime.strptime(ccb1['交易日期'][i], '%Y%m%d').strftime('%m/%d/%Y')# 将交易日期改成pandas可识别的日期格式
+    #for i in ccb1.index:
+        #ccb1['交易日期'][i] = datetime.strptime(ccb1['交易日期'][i], '%Y%m%d').strftime('%m/%d/%Y')# 将交易日期改成pandas可识别的日期格式
     ccb1.iloc[:, 0] = ccb1.iloc[:, 0].astype('datetime64')  # 数据类型更改
     ccb1.iloc[:, 1] = ccb1.iloc[:, 1].astype('datetime64')  # 数据类型更改
     ccb1.iloc[:, 2] = ccb1.iloc[:, 2].astype('float64')  # 数据类型更改
@@ -22,9 +21,57 @@ def read_data_ccb1(path):  # 获取建设银行储蓄卡数据
     len1 = len(ccb1)
 
     ccb1.insert(7, '来源', "建设银行储蓄卡", allow_duplicates=True)  # 添加建设银行储蓄卡来源标识
+    #print("成功读取 " + str(len1) + " 条「建设银行储蓄卡」账单数据\n")
+    #print(ccb1)
+    #print(ccb1.dtypes)
+    return(ccb1)ß
 
-    print("成功读取 " + str(len1) + " 条「建设银行储蓄卡」账单数据\n")
-    print(ccb1)
-    print(ccb1.dtypes)
+def read_data_ccb2(path):  # 获取建设银行信用卡数据
+    ccb2 = pd.read_csv(path,header=3, skipfooter=0, encoding='GB2312')
+    ccb2 = ccb2.iloc[:, [0, 5, 3, 6]]  # 按顺序提取所需列
+    ccb2 = strip_in_data(ccb2)  # 去除列名与数值中的空格。
+    ccb2.rename(columns={'交易日': '交易日期', '入账金额': '支出', '类型': '摘要','交易描述': '交易详情'}, inplace=True)  # 修改列名称
+    ccb2.insert(1, '交易时间', "00:00:00")  
+    ccb2.insert(3, '收入', "")  
+    ccb2.insert(5, '对方户名', "无")
+    ccb2.insert(7, '数据来源', "建设银行信用卡")# 添加建设银行信用卡来源标识
+    ccb2.iloc[:, 0] = ccb2.iloc[:, 0].astype('str')  # 数据类型更改
+    ccb2.iloc[:, 0] = ccb2.iloc[:, 0].astype('datetime64')  # 数据类型更改
+    ccb2.iloc[:, 1] = ccb2.iloc[:, 1].astype('datetime64')  # 数据类型更改
+    ccb2.iloc[:, 2] = ccb2.iloc[:, 2].astype('float64')  # 数据类型更改
+    for i in ccb2.index:       #将“支出/收入”单列转换为支出、收入分别一列
+        if ccb2.iloc[i,2] < 0:
+            ccb2.iloc[i,3]= -ccb2.iloc[i,2]
+            ccb2.iloc[i,2]= 0
+        else:
+            ccb2.iloc[i,3]= 0 
+    
+    ccb2.iloc[:, 3] = ccb2.iloc[:, 3].astype('float64')  # 数据类型更改
+    #print(ccb2)
+    #print(ccb2.dtypes)
+    len2 = len(ccb2)
+    #print("成功读取 " + str(len2) + " 条「建设银行信用卡」账单数据\n") 
+    return(ccb2)
 
-read_data_ccb1('储蓄卡账单.txt')
+'''
+path1 = '储蓄卡账单.txt'
+path2 = '信用卡账单.csv'
+path_account = '个人财务管理.xlsx'
+
+data_ccb1 = read_data_ccb1(path1)
+data_ccb2 = read_data_ccb2(path2)
+
+data_merge = pd.concat([data_ccb1,data_ccb2])
+merge_list = data_merge.values.tolist()
+
+
+workbook = openpyxl.load_workbook(path_account)  # openpyxl读取账本文件
+sheet = workbook['财务记录']
+maxrow = sheet.max_row  # 获取最大行
+print('\n「明细」 sheet 页已有 ' + str(maxrow) + ' 行数据，将在末尾写入数据')
+for row in merge_list:
+    sheet.append(row)  # openpyxl写文件
+
+workbook.save(path_account)  # 保存
+print("\n成功将数据写入到 " + path_account)
+'''
--- test.py
+++ test.py
@@ -1,10 +1,25 @@
 
-import imp
 import pandas as pd
 import numpy as np
 from datetime import datetime
 
 
-a = '20220101'
-a = datetime.strptime(a, '%Y%m%d').strftime('%Y-%m-%d')
-print(a)
+
+path = '信用卡账单.csv'
+ccb2 = pd.read_csv(path,header=3, skipfooter=0, encoding='GB2312')
+ccb2 = ccb2.iloc[:, [0, 5, 5, 3, 6]]  # 按顺序提取所需列
+ccb2 = strip_in_data(ccb2)  # 去除列名与数值中的空格。
+print(ccb2.dtypes)
+ccb2.iloc[:, 0] = ccb2.iloc[:, 0].astype('str')  # 数据类型更改
+#for i in ccb2.index:
+    #ccb2['交易日期'][i] = datetime.strptime(ccb2['交易日期'][i], '%Y%m%d').strftime('%m/%d/%Y')# 将交易日期改成pandas可识别的日期格式
+ccb2.iloc[:, 0] = ccb2.iloc[:, 0].astype('datetime64')  # 数据类型更改
+ccb2.iloc[:, 1] = ccb2.iloc[:, 1].astype('datetime64')  # 数据类型更改
+ccb2.iloc[:, 2] = ccb2.iloc[:, 2].astype('float64')  # 数据类型更改
+ccb2.iloc[:, 3] = ccb2.iloc[:, 3].astype('float64')  # 数据类型更改
+len1 = len(ccb2)
+
+ccb2.insert(7, '来源', "建设银行储蓄卡", allow_duplicates=True)  # 添加建设银行储蓄卡来源标识
+print("成功读取 " + str(len1) + " 条「建设银行储蓄卡」账单数据\n")
+print(ccb2)
+print(ccb2.dtypes)
