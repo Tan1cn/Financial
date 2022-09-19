d821e49845ab4f3270a9bc4236939477ea98e52a
Tan1 <tanyimailcn@gmail.com>
2022-09-19 14:16:02 +0800

    1

--- Account.py
+++ Account.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import openpyxl
-from datetime import datetime
+import datetime
+
 
 def strip_in_data(data):  # 把列名中和数据中首尾的空格都去掉。
     data = data.rename(columns={column_name: column_name.strip() for column_name in data.columns})
@@ -12,19 +13,21 @@ def read_data_ccb1(path):  # 获取建设银行储蓄卡数据
     ccb1 = ccb1.iloc[:, [1, 2, 3, 4, 7, 9, 10]]  # 按顺序提取所需列
     ccb1 = strip_in_data(ccb1)  # 去除列名与数值中的空格。
     ccb1.iloc[:, 0] = ccb1.iloc[:, 0].astype('str')  # 数据类型更改
-    #for i in ccb1.index:
-        #ccb1['交易日期'][i] = datetime.strptime(ccb1['交易日期'][i], '%Y%m%d').strftime('%m/%d/%Y')# 将交易日期改成pandas可识别的日期格式
     ccb1.iloc[:, 0] = ccb1.iloc[:, 0].astype('datetime64')  # 数据类型更改
     ccb1.iloc[:, 1] = ccb1.iloc[:, 1].astype('datetime64')  # 数据类型更改
     ccb1.iloc[:, 2] = ccb1.iloc[:, 2].astype('float64')  # 数据类型更改
     ccb1.iloc[:, 3] = ccb1.iloc[:, 3].astype('float64')  # 数据类型更改
-    len1 = len(ccb1)
+    ccb1.rename(columns={'交易地点': '交易详情'}, inplace=True)  # 修改列名称
+    ccb1.insert(7, '数据来源', "建设银行储蓄卡", allow_duplicates=True)  # 添加建设银行储蓄卡来源标识
+    for i in ccb1.index:       #将收入中的空值转换为0
+        if bool(pd.isnull(ccb1.iloc[i,3])):
+            ccb1.iloc[i,3]= 0
 
-    ccb1.insert(7, '来源', "建设银行储蓄卡", allow_duplicates=True)  # 添加建设银行储蓄卡来源标识
-    #print("成功读取 " + str(len1) + " 条「建设银行储蓄卡」账单数据\n")
+    len1 = len(ccb1)
+    print("成功读取 " + str(len1) + " 条「建设银行储蓄卡」账单数据\n")
     #print(ccb1)
     #print(ccb1.dtypes)
-    return(ccb1)ß
+    return(ccb1)
 
 def read_data_ccb2(path):  # 获取建设银行信用卡数据
     ccb2 = pd.read_csv(path,header=3, skipfooter=0, encoding='GB2312')
@@ -50,10 +53,10 @@ def read_data_ccb2(path):  # 获取建设银行信用卡数据
     #print(ccb2)
     #print(ccb2.dtypes)
     len2 = len(ccb2)
-    #print("成功读取 " + str(len2) + " 条「建设银行信用卡」账单数据\n") 
+    print("成功读取 " + str(len2) + " 条「建设银行信用卡」账单数据\n") 
     return(ccb2)
 
-'''
+
 path1 = '储蓄卡账单.txt'
 path2 = '信用卡账单.csv'
 path_account = '个人财务管理.xlsx'
@@ -62,8 +65,8 @@ data_ccb1 = read_data_ccb1(path1)
 data_ccb2 = read_data_ccb2(path2)
 
 data_merge = pd.concat([data_ccb1,data_ccb2])
+print(data_merge.dtypes)
 merge_list = data_merge.values.tolist()
-
 
 workbook = openpyxl.load_workbook(path_account)  # openpyxl读取账本文件
 sheet = workbook['财务记录']
@@ -74,4 +77,3 @@ for row in merge_list:
 
 workbook.save(path_account)  # 保存
 print("\n成功将数据写入到 " + path_account)
-'''
