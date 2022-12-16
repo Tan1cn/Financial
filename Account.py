c14896591f3f5f52a7d6b6f8c14f05a5663e48ed
Tan1 <tanyimailcn@gmail.com>
2022-12-16 22:57:05 +0800

    Update Account.py

--- Account.py
+++ Account.py
@@ -3,8 +3,10 @@ import openpyxl
 import datetime
 import os
 
-path1 = r'C:\Users\tanyi\Downloads\储蓄卡账单.txt'
-path2 = r'C:\Users\tanyi\Downloads\信用卡账单.csv'
+path1 = r'C:\Users\tanyi\Downloads\储蓄卡账单.txt'                          #建设银行储蓄卡账单路径（txt文件）
+path2 = r'C:\Users\tanyi\Downloads\信用卡账单.csv'                          #建设银行信用卡账单路径（csv文件）
+path3 = r'D:\Python\Financial\Data\微信支付账单(20220921-20221209).csv'     #微信账单（csv文件）
+path4 = r''                                                                 #支付宝账单（csv文件）
 
 path_account = r'D:\@生活\台帐\个人财务管理.xlsx'
 path_merge = r'本次写入数据.csv'
@@ -17,7 +19,7 @@ def strip_in_data(data):  # 把列名中和数据中首尾的空格都去掉。
     return data
 
 
-def read_data_ccb1(path):  # 获取建设银行储蓄卡数据
+def read_data_ccb1(path):  # 获取建设银行储蓄卡数据,并调整数据格式
     ccb1 = pd.read_csv(path,header=3, skipfooter=0, encoding='utf-8')
     ccb1 = ccb1.iloc[:, [1, 2, 3, 4, 7, 9, 10]]  # 按顺序提取所需列
     ccb1 = strip_in_data(ccb1)  # 去除列名与数值中的空格。
@@ -29,17 +31,13 @@ def read_data_ccb1(path):  # 获取建设银行储蓄卡数据
     ccb1.rename(columns={'交易地点': '交易详情'}, inplace=True)  # 修改列名称
     ccb1.insert(7, '数据来源', "建设银行储蓄卡", allow_duplicates=True)  # 添加建设银行储蓄卡来源标识
     ccb1 = ccb1.fillna(0)
-    #for i in ccb1.index:       #将收入中的空值转换为0
-    #    if bool(pd.isnull(ccb1.iloc[i,3])):
-    #        ccb1.iloc[i,3]= 0
-
     len1 = len(ccb1)
     print("成功读取 " + str(len1) + " 条「建设银行储蓄卡」账单数据\n")
     #print(ccb1)
     #print(ccb1.dtypes)
     return(ccb1)
 
-def read_data_ccb2(path):  # 获取建设银行信用卡数据
+def read_data_ccb2(path):  # 获取建设银行储蓄卡数据,并调整数据格式
     ccb2 = pd.read_csv(path,header=3, skipfooter=0, encoding='GB2312',encoding_errors='ignore')
     ccb2 = ccb2.iloc[:, [0, 5, 3, 6]]  # 按顺序提取所需列
     ccb2 = strip_in_data(ccb2)  # 去除列名与数值中的空格。
@@ -58,7 +56,6 @@ def read_data_ccb2(path):  # 获取建设银行信用卡数据
             ccb2.iloc[i,2]= 0
         else:
             ccb2.iloc[i,3]= 0 
-    
     ccb2.iloc[:, 3] = ccb2.iloc[:, 3].astype('float64')  # 数据类型更改
     #print(ccb2)
     #print(ccb2.dtypes)
@@ -66,18 +63,53 @@ def read_data_ccb2(path):  # 获取建设银行信用卡数据
     print("成功读取 " + str(len2) + " 条「建设银行信用卡」账单数据\n") 
     return(ccb2)
 
+def read_data_wx1(path):  # 获取微信数据,并调整数据格式
+    wx1 = pd.read_csv(path,header=16, skipfooter=0, encoding='utf_8',encoding_errors='ignore')
+    wx1.iloc[:, 5] = wx1.iloc[:, 5].str[1:].astype('float64')  # 数据类型更改
+    for i in wx1.index:       #将收入赋负号
+        if wx1.iloc[i,4] == '收入':
+            wx1.iloc[i,5]= - wx1.iloc[i,5]
+    wx1 = wx1[ ~ wx1['支付方式'].str.contains('银行') ]
+    wx1 = wx1.iloc[:, [0, 0, 5, 1, 2, 3]]  # 按顺序提取所需列
+    wx1 = strip_in_data(wx1)  # 去除列名与数值中的空格。
+    wx1.columns = ['交易日期','交易时间','支出','摘要','对方户名','交易详情']
+    wx1.insert(3, '收入', "")  
+    wx1.insert(7, '数据来源', "微信")# 添加微信来源标识
+    wx1.iloc[:, 0] = wx1.iloc[:, 0].astype('str')  # 数据类型更改
+    wx1.iloc[:, 0] = wx1.iloc[:, 0].astype('datetime64')  # 数据类型更改
+    wx1.iloc[:, 1] = wx1.iloc[:, 1].astype('datetime64')  # 数据类型更改
+    wx1.iloc[:, 2] = wx1.iloc[:, 2].astype('float64')  # 数据类型更改
+    wx1.reset_index(drop=True, inplace=True)
 
+    for i in wx1.index:       #将“支出/收入”单列转换为支出、收入分别一列
+        if wx1.iloc[i,2] < 0:
+            wx1.iloc[i,3]= -wx1.iloc[i,2]
+            wx1.iloc[i,2]= 0
+        else:
+            wx1.iloc[i,3]= 0 
+    wx1.iloc[:, 3] = wx1.iloc[:, 3].astype('float64')  # 数据类型更改
+    #print(wx1)
+    #print(wx1.dtypes)
+    len2 = len(wx1)
+    print("成功读取 " + str(len2) + " 条「微信」账单数据\n") 
+    return(wx1)
 
-data_ccb1 = read_data_ccb1(path1)
-data_ccb2 = read_data_ccb2(path2)
+#---------------------------------------------------------------------------------------------------
 
-data_merge = pd.concat([data_ccb1,data_ccb2])
+if os.path.exists(path1):
+    data_ccb1 = read_data_ccb1(path1)
+elif os.path.exists(path2):
+    data_ccb2 = read_data_ccb2(path2)
+elif os.path.exists(path3):
+    data_wx1 = read_data_wx1(path3)
+
+data_merge = pd.concat([data_ccb1,data_ccb2,data_wx1])
 print(data_merge)
 merge_list = data_merge.values.tolist()
 
 data_merge.to_csv(path_merge)
 
-
+#---------------------------------------------------------------------------------------------------
 
 workbook = openpyxl.load_workbook(path_account)  # openpyxl读取账本文件
 sheet = workbook['财务记录']
